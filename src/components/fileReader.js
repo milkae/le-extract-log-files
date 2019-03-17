69987dbdd57f59dbbc8dd97ed8b33bc43393b4ce
Milkae <floriane@tuta.io>
2019-03-17 01:58:10 +0100

    Remove lines matching excluded patterns

--- src/components/fileReader.js
+++ src/components/fileReader.js
@@ -1,21 +1,60 @@
 import React from "react"
 
-const FileInput = () => {
+const FileInput = ({ setText }) => {
   let fileReader
   const testLine = line => {
-    const date = '\\[\\d{2}:\\d{2}:\\d{2}\\]'
-    const linesExcluded = [ `${date} Pour remercier les aventuriers`]
-    return linesExcluded.every(exclude => {
+    const date = "\\[\\d{2}:\\d{2}:\\d{2}\\]"
+    const linesExcluded = [
+      `${date} $`,
+      `${date} Pour `,
+      `${date} Fichiers `,
+      `${date} Tu `,
+      `${date} La `,
+      `${date} Le `,
+      `${date} En `,
+      `${date} Je `,
+      `${date} Tes `,
+      `${date} \\[MP`,
+      `${date} Heure `,
+      `${date} Jour `,
+      `${date} Trop `,
+      `${date} Ton `,
+      `${date} Bien, `,
+      `${date} Vers `,
+      `${date} Un `,
+      `${date} Cette `,
+      `${date} Vous `,
+      `${date} #Mg `,
+      `${date} Utilise `,
+      `${date} Continue `,
+      "Qu.te globale",
+      "D.but du journal",
+      "^Jour ",
+      "^En ce jour",
+      "^La ",
+      "@ 1]:",
+      "@ 3]:",
+      "Hourly time-stamp",
+      "a été notifié, maintenant attends qu'il veuille n.gocier avec toi$",
+      "a abandonné la n.gociation.$",
+      "vient de se connecter.$",
+      "vient de se d.connecter.$"
+    ]
+
+    return (
+      line &&
+      linesExcluded.every(exclude => {
         const regex = new RegExp(exclude)
-        return !regex.test(line);
-    })
+        return !regex.test(line)
+      })
+    )
   }
 
   const handleFileRead = e => {
     const content = fileReader.result
     const lines = content.split(/[\r\n]+/g)
     const filteredLines = lines.filter(testLine)
-    console.log(filteredLines)
+    setText(filteredLines.join("\n"))
   }
 
   const handleFile = file => {
