3f3ba4fe767854e9c697d30721392930d5fd943a
Milkae <floriane@tuta.io>
2019-03-17 01:20:48 +0100

    Search and remove a specific line from the file

--- src/components/fileReader.js
+++ src/components/fileReader.js
@@ -1,27 +1,40 @@
 import React from "react"
 
 const FileInput = () => {
-    let fileReader;
+  let fileReader
+  const testLine = line => {
+    const date = '\\[\\d{2}:\\d{2}:\\d{2}\\]'
+    const linesExcluded = [ `${date} Pour remercier les aventuriers`]
+    return linesExcluded.every(exclude => {
+        const regex = new RegExp(exclude)
+        return !regex.test(line);
+    })
+  }
 
-    const handleFileRead = (e) => {
-        const content = fileReader.result;
-        console.log(content);
-    };
+  const handleFileRead = e => {
+    const content = fileReader.result
+    const lines = content.split(/[\r\n]+/g)
+    const filteredLines = lines.filter(testLine)
+    console.log(filteredLines)
+  }
 
-    const handleFile = (file) => {
-        fileReader = new FileReader();
-        fileReader.onloadend = handleFileRead;
-        fileReader.readAsText(file);
-    };
+  const handleFile = file => {
+    fileReader = new FileReader()
+    fileReader.onloadend = handleFileRead
+    fileReader.readAsText(file)
+  }
 
-    return <div>
-        <input type='file'
-            id='file'
-            className='input-file'
-            accept='.txt'
-            onChange={e => handleFile(e.target.files[0])}
-        />
-    </div>;
-};
+  return (
+    <div>
+      <input
+        type="file"
+        id="file"
+        className="input-file"
+        accept=".txt"
+        onChange={e => handleFile(e.target.files[0])}
+      />
+    </div>
+  )
+}
 
 export default FileInput
